8d9205cde808f4f61fbc69015c254e6a5d76d7cb
rita-snow <yummy.standrews@gmail.com>
2024-12-28 12:08:59 +0900

    usersテーブルの修正

--- db/migrate/20241226232939_devise_create_users.rb
+++ db/migrate/20241226232939_devise_create_users.rb
@@ -1,12 +1,11 @@
-# frozen_string_literal: true
-
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :employee_number,         null: false, unique: true
+      t.integer :role,                   null: false
       t.string :user_name,               null: false
-      # t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :encrypted_password,      null: false, default: ""
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -32,11 +31,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
       t.timestamps null: false
     end
 
     # add_index :users, :email,                unique: true
+    add_index :users, :employee_number,      unique: true # 社員番号の一意性を保証
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
