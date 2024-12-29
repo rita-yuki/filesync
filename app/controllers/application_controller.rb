528eefc35d3c6b2305c881007546b667c99a6885
rita-snow <yummy.standrews@gmail.com>
2024-12-29 18:44:26 +0900

    リダイレクト先を設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  def after_sign_in_path_for(resource)
+    documents_path
+  end
 
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:employee_number, :user_name, :role])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:employee_number, :user_name, :role])
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path 
   end
 end
