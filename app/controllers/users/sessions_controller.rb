1d26009506860a00c48e037402931a5425272d5a
rita-snow <yummy.standrews@gmail.com>
2024-12-29 18:47:17 +0900

    ログイン済み時エラーメッセージを表示

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,14 +1,11 @@
 class Users::SessionsController < Devise::SessionsController
-  def create
-    self.resource = User.find_by(employee_number: params[:user][:employee_number])
-    if resource&.valid_password?(params[:user][:password])
-      set_flash_message!(:notice, :signed_in)
-      sign_in(resource_name, resource)
-      yield resource if block_given?
-      respond_with resource, location: after_sign_in_path_for(resource)
-    else
-      set_flash_message!(:alert, :invalid)
-      redirect_to new_session_path(resource_name)
+  before_action :redirect_if_authenticated, only: [:new]
+
+  private
+
+  def redirect_if_authenticated
+    if user_signed_in?
+      redirect_to documents_path, alert: '既にログインしています。'
     end
   end
 end
